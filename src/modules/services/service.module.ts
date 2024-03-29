e241ca5fe6aab0c68826b60fa54869057aaa7949
notmhmd <mhmdfrj.97@gmail.com>
2024-03-29 23:28:32 +0400

    adding pi module to service module

--- src/modules/services/service.module.ts
+++ src/modules/services/service.module.ts
@@ -6,9 +6,10 @@ import ServiceController from "./service.controller";
 import ServiceEntity from "./entities/service.entity";
 import { AuthModule } from "../auth/auth.module";
 import PromoteOrderEntity from "./entities/promote-order.entity";
+import { PiModule } from "../pi/pi.module";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([ServiceTypeEntity, ServiceEntity, PromoteOrderEntity]), AuthModule],
+    imports: [TypeOrmModule.forFeature([ServiceTypeEntity, ServiceEntity, PromoteOrderEntity]), AuthModule, PiModule],
     exports: [ServiceService],
     providers: [ServiceService],
     controllers: [ServiceController],
