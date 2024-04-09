bc1a152387141dfde9b15ec741d09a2d471f50fe
notmhmd <mhmdfrj.97@gmail.com>
2024-04-09 23:19:32 +0400

    debug: payment dto

--- src/modules/pi/dto/request.ts
+++ src/modules/pi/dto/request.ts
@@ -1,28 +1,95 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsBoolean, IsObject, IsOptional, IsString, ValidateNested } from "class-validator";
+import { Type } from "class-transformer";
+export class PaymentStatus{
+
+  @ApiProperty()
+  @IsBoolean()
+  developer_approved: boolean; // Server-Side Approval
+
+  @ApiProperty()
+  @IsBoolean()
+  transaction_verified: boolean; // blockchain transaction verified
+
+  @ApiProperty()
+  @IsBoolean()
+  developer_completed: boolean; // Server-Side Completion
+
+  @ApiProperty()
+  @IsBoolean()
+  cancelled: boolean// cancelled by the developer or by Pi Network
+
+  @ApiProperty()
+  @IsBoolean()
+  user_cancelled: boolean // cancelled by the user
+
+}
+
+
+export class PaymentTransaction {
+  // This is null if no transaction has been made yet
+  @ApiProperty()
+  @IsString()
+  txid: string // id of the blockchain transaction
+
+  @ApiProperty()
+  @IsBoolean()
+  verified: boolean // true if the transaction matches the payment, false otherwise
+
+  @ApiProperty()
+  @IsString()
+  _link: string // a link to the operation on the Blockchain API
+}
 export class PaymentDTO {
   // Payment data:
+  @ApiProperty()
+  @IsString()
   identifier: string;
+
+  @ApiProperty()
+  @IsString()
     user_uid: string;
+
+  @ApiProperty()
+  @IsString()
   amount: number;
+
+  @ApiProperty()
+  @IsString()
   memo: string; // a string provided by the developer, shown to the user
-  metadata: Object; // an object provided by the developer for their own usage
+
+
+  @ApiProperty()
+  @IsObject()
+  metadata: any; // an object provided by the developer for their own usage
+
+  @ApiProperty()
+  @IsString()
   from_address: string; // sender address of the blockchain transaction
+
+  @ApiProperty()
+  @IsString()
   to_address: string; // recipient address of the blockchain transaction
+
+  @ApiProperty()
+  @IsString()
   direction: any; // direction of the payment
+
+  @ApiProperty()
+  @IsString()
   created_at: Date; // the payment's creation timestamp
 
   // Status flags representing the current state of this payment
-  status: {
-  developer_approved: boolean; // Server-Side Approval
-    transaction_verified: boolean; // blockchain transaction verified
-    developer_completed: boolean; // Server-Side Completion
-    cancelled: boolean, // cancelled by the developer or by Pi Network
-    user_cancelled: boolean, // cancelled by the user
-};
+  @ApiProperty()
+  @ValidateNested()
+  @Type(() => PaymentStatus)
+  status: PaymentStatus;
 
   // Blockchain transaction data:
-  transaction?: { // This is null if no transaction has been made yet
-    txid: string, // id of the blockchain transaction
-    verified: boolean, // true if the transaction matches the payment, false otherwise
-    _link: string, // a link to the operation on the Blockchain API
-  }
-};
+  @ApiProperty()
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => PaymentTransaction)
+  transaction?: PaymentTransaction
+}
+
